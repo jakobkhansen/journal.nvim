76184c9618fc2b50d6b389cab63983e9eacefa9a
Jakob Hansen <jakob.hansen@hotmail.no>
2024-04-09 22:26:29 +0200

    feat: Yesterday and relative dates

--- lua/journal/date.lua
+++ lua/journal/date.lua
@@ -1,26 +1,60 @@
 local M = {}
 -- Meta class
-Date = {day = 0, month = 0, year = 0}
+Date = { day = 0, month = 0, year = 0, wday = 0 }
 
 -- Derived class method new
 
-function Date:new (o, day, month, year)
-   o = o or {}
-   setmetatable(o, self)
-   self.__index = self
-   self.day = day or 0
-   self.month = month or 0
-   self.year = year
-   return o
-end
-
-function Date:today (o)
-    o = o or {}
+function Date:new(day, month, year)
+    o = {}
     setmetatable(o, self)
     self.__index = self
-    self.day = os.date("%d")
-    self.month = os.date("%m")
-    self.year = os.date("%Y")
+    self.day = day or 0
+    self.month = month or 0
+    self.year = year
+    return o
+end
+
+function Date:today()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    local data = os.date("*t")
+    self.day = data.day
+    self.month = data.month
+    self.year = data.year
+    self.wday = data.wday
+    return o
+end
+
+function Date:yesterday()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+
+    local today = os.date("*t")
+    today.day = today.day - 1
+    local yesterday = os.date("*t", os.time(today))
+
+    self.day = yesterday.day
+    self.month = yesterday.month
+    self.year = yesterday.year
+    self.wday = yesterday.wday
+    return o
+end
+
+function Date:relative(date)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+
+    local today = os.date("*t")
+    today.day = today.day + date
+    local relative = os.date("*t", os.time(today))
+
+    self.day = relative.day
+    self.month = relative.month
+    self.year = relative.year
+    self.wday = relative.wday
     return o
 end
 
--- lua/journal/dateparser.lua
+++ lua/journal/dateparser.lua
@@ -8,6 +8,12 @@ M.parse_date = function(date)
     if (string.lower(date[1]) == 'today') then
         return Date:today()
     end
+    if (string.lower(date[1]) == 'yesterday') then
+        return Date:yesterday()
+    end
+    if (date[1]:sub(1, 1) == '-' or date[1]:sub(1, 1) == '+') then
+        return Date:relative(tonumber(date[1]))
+    end
 
     return nil
 end
