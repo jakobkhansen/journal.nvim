ecdcff8ebaa42f08e45135d759492e2ffcc60245
Jakob Hansen <jakob.hansen@hotmail.no>
2024-04-14 00:52:04 +0200

    refactor: Naming convention

--- lua/journal/filesystem.lua
+++ lua/journal/filesystem.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local config = require('journal.config').get()
 
-local function createDirectories(path)
+local function create_directories(path)
     local success, error = os.execute(string.format("mkdir -p $(dirname %s)", path))
     if not success then
         return nil, error
@@ -10,33 +10,33 @@ local function createDirectories(path)
     return true
 end
 
-local function getEntryPath(date, format)
+local function get_entry_path(date, format)
     local journal_dir = vim.fn.expand(config.dir)
     return journal_dir .. '/' .. os.date(format, os.time(date)) .. '.' .. config.filetype
 end
 
-M.open_entry = function(entry)
-    createDirectories(entry)
+M.open_entry = function(entry, template)
+    create_directories(entry)
     vim.cmd('edit ' .. entry)
 end
 
 M.open_day_entry = function(date)
-    local journal_file = getEntryPath(date, config.day_entry_format)
+    local journal_file = get_entry_path(date, config.day_entry_format)
     M.open_entry(journal_file)
 end
 
 M.open_week_entry = function(date)
-    local journal_file = getEntryPath(date, config.week_entry_format)
+    local journal_file = get_entry_path(date, config.week_entry_format)
     M.open_entry(journal_file)
 end
 
 M.open_month_entry = function(date)
-    local journal_file = getEntryPath(date, config.month_entry_format)
+    local journal_file = get_entry_path(date, config.month_entry_format)
     M.open_entry(journal_file)
 end
 
 M.open_year_entry = function(date)
-    local journal_file = getEntryPath(date, config.year_format)
+    local journal_file = get_entry_path(date, config.year_format)
     M.open_entry(journal_file)
 end
 
