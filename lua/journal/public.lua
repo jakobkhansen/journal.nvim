975ee7354880fc304e2aa1f49a6912e16f4c3767
Jakob Hansen <jakob.hansen@hotmail.no>
2024-05-04 19:04:58 +0200

    feat: Public method for opening entry given date and config

--- lua/journal/public.lua
+++ lua/journal/public.lua
@@ -1,11 +1,18 @@
 local M = {}
 
 local command = require("journal.command")
+local fs = require("journal.filesystem")
 
 local utils = require("journal.utils")
 
+-- Executes :Journal command with the given string as args
 M.command = function(string)
     command.execute(utils.split(string, "%S+"))
+end
+
+-- Opens a journal entry for the given date and config
+M.open_entry = function(date, config)
+    fs.open_entry(date, config)
 end
 
 return M
